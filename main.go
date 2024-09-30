f78ed8ca64469d4ca70f4cb736fed14f7c54acfb
cz98265318 <oleksandr.novozhylov@tesco.com>
2024-09-30 22:52:17 +0200

    Add all the pages from new template

--- main.go
+++ main.go
@@ -16,9 +16,128 @@ var templatesFS embed.FS
 //go:embed static/*
 var staticFS embed.FS
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	//tpl, err := template.ParseFS(templatesFS, "templates/home.html")
 	tpl, err := template.ParseFS(templatesFS, "templates/index.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/about.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/contact.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func priceHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/price.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/projects.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func servicesHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/services.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func sidebarHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/sidebar-right.html")
+
+	if err != nil {
+		http.Error(w, "Could not load template", http.StatusInternalServerError)
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Could not execute template", http.StatusInternalServerError)
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFS(templatesFS, "templates/home.html")
 
 	if err != nil {
 		http.Error(w, "Could not load template", http.StatusInternalServerError)
@@ -52,7 +171,32 @@ func main() {
 
 	http.HandleFunc("/static/", staticFileHandler)
 
-	http.HandleFunc("/", homeHandler)
+	//http.HandleFunc("/", homeHandler)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/":
+			indexHandler(w, r)
+		case "/index.html":
+			indexHandler(w, r)
+		case "/about.html":
+			aboutHandler(w, r)
+		case "/contact.html":
+			contactHandler(w, r)
+		case "/price.html":
+			priceHandler(w, r)
+		case "/projects.html":
+			projectsHandler(w, r)
+		case "/services.html":
+			servicesHandler(w, r)
+		case "/sidebar-right.html":
+			sidebarHandler(w, r)
+		case "/home.html":
+			homeHandler(w, r)
+		default:
+			http.NotFound(w, r)
+		}
+	})
 
 	fmt.Println("Starting server on http://127.0.0.1:80")
 
